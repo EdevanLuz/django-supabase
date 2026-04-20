8a4cbae7353b9dcde8e9858881824b4914674b18
Edevan <edevanluz@gmail.com>
2026-04-20 07:16:48 -0300

    feat/init

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Usuario
 
-# Register your models here.
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "nome",
+        "eh_admin",
+        "esta_ativo",
+        "criado_em",
+    )
+    list_filter = ("eh_admin", "esta_ativo", "criado_em")
+    search_fields = ("email", "nome")
+    ordering = ("-criado_em",)
